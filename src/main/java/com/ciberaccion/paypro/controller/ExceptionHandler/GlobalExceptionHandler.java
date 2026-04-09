a3060766d7d0f5e152a10ee067f92457df89f24f
Ciberaccion <argranados@yahoo.com>
2026-04-09 10:09:37 -0600

    add custom exception

--- src/main/java/com/ciberaccion/paypro/controller/ExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/ciberaccion/paypro/controller/ExceptionHandler/GlobalExceptionHandler.java
@@ -5,8 +5,12 @@ import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.ciberaccion.paypro.controller.exception.PaymentNotFoundException;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -16,6 +20,14 @@ public class GlobalExceptionHandler {
             errors.put(error.getField(), error.getDefaultMessage())
         );
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    // agrega este nuevo
+    @ExceptionHandler(PaymentNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleNotFound(PaymentNotFoundException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        return ResponseEntity.status(404).body(error);
     }
 
 }
